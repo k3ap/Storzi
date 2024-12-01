599687a0484fe00653c3ea9a65cd29c00063eb18
Filip Štamcar <filip.stamcar@hotmail.com>
2024-12-01 19:32:02 +0100

    Update debug patterns to new format

--- patterns/debug-pattern-1/main.py
+++ patterns/debug-pattern-1/main.py
@@ -1,7 +1,9 @@
 import time
+
 print('#{"version": 0, "led_count": 500, "fps": 60}')
-while True
-    print("#" + "00ff00"*500)
+
+while True:
+    print("#" + "ff0000" * 500)
     time.sleep(1)
-    print("#" + "ff00ff"*500)
+    print("#" + "0000ff" * 500)
     time.sleep(1)
